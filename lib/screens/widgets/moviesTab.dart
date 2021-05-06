97e1d6f1a0cb02776d97add115bcb6475723ff36
lcyper <lcyper@gmail.com>
2021-05-06 19:40:11 -0300

    refactoring moviesTab

--- lib/screens/widgets/moviesTab.dart
+++ lib/screens/widgets/moviesTab.dart
@@ -10,45 +10,17 @@ class _MoviesTabState extends State<MoviesTab> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      initialIndex: 1,
+      initialIndex: 0,
       length: 2, //3
       child: Column(
         children: [
           SizedBox(
             height: 40.0,
-            child: TabBar(
-              indicatorColor: Colors.yellow,
-              indicatorWeight: 5.0,
-              indicatorPadding: EdgeInsets.only(
-                left: 50.0,
-                right: 50.0,
-              ),
-              labelStyle: TextStyle(fontWeight: FontWeight.bold),
-              unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-              tabs: [
-                // Tab(
-                //   icon: Icon(
-                //     Icons.favorite,
-                //   ),
-                // ),
-                Tab(
-                  child: Text(
-                    'Populares',
-                    style: _textStyleLabels,
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    'Mejor Puntuadas',
-                    style: _textStyleLabels,
-                  ),
-                ),
-              ],
-            ),
+            child: _buildTabBar(),
           ),
           Expanded(
             child: TabBarView(
-              physics: BouncingScrollPhysics(),
+              // physics: BouncingScrollPhysics(),
               children: [
                 // Icon(Icons.favorite_border),
                 getMoviesPage('movie/popular'),
@@ -61,7 +33,39 @@ class _MoviesTabState extends State<MoviesTab> {
     );
   }
 
-  TextStyle _textStyleLabels = TextStyle(
+  TabBar _buildTabBar() {
+    return TabBar(
+      indicatorColor: Colors.yellow,
+      indicatorWeight: 5.0,
+      indicatorPadding: EdgeInsets.only(
+        left: 50.0,
+        right: 50.0,
+      ),
+      labelStyle: TextStyle(fontWeight: FontWeight.bold),
+      unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+      tabs: [
+        // Tab(
+        //   icon: Icon(
+        //     Icons.favorite,
+        //   ),
+        // ),
+        Tab(
+          child: Text(
+            'Populares',
+            style: _textStyleTab,
+          ),
+        ),
+        Tab(
+          child: Text(
+            'Mejor Puntuadas',
+            style: _textStyleTab,
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextStyle _textStyleTab = TextStyle(
     color: Colors.black,
     fontSize: 20.0,
   );
