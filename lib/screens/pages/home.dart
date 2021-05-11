c550f6dd9bbd6b9b4e7448653a0dcc0c9c8e14db
lcyper <lcyper@gmail.com>
2021-05-11 00:57:02 -0300

    mejora bottomNavigatorBar y boton de favoritos

--- lib/screens/pages/home.dart
+++ lib/screens/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:themoviedb/screens/pages/favoritesPage.dart';
 import 'package:themoviedb/screens/widgets/findMoviesTab.dart';
 import 'package:themoviedb/screens/widgets/moviesTab.dart';
+// import 'package:themoviedb/class/Movies.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,32 +19,80 @@ class _HomeState extends State<Home> {
         child: _selectedTabIndex == 0 ? MoviesTab() : FindMoviesTab(),
       ),
       bottomNavigationBar: _buildBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Favoritos',
+        elevation: 3.0,
+        mini: true,
+        backgroundColor: Colors.red,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FavoritesPage(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.favorite,
+          size: 30.0,
+          // color: Colors.white,
+        ),
+      ),
     );
   }
 
-  BottomNavigationBar _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.black,
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.movie_outlined),
-          label: 'pelis',
-          backgroundColor: Colors.black,
-          activeIcon: Icon(Icons.movie),
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.find_in_page_outlined),
-          activeIcon: Icon(Icons.find_in_page),
-          label: 'buscar',
-        )
-      ],
-      currentIndex: _selectedTabIndex,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey[400],
-      onTap: _onBottomMenuTapped,
-      // showSelectedLabels: false,
-      showUnselectedLabels: false,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black38,
+            spreadRadius: 0,
+            blurRadius: 7,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+        child: BottomAppBar(
+          notchMargin: 5.0,
+          shape: CircularNotchedRectangle(),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.grey[180],
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.movie_outlined),
+                label: 'pelis',
+                // backgroundColor: Colors.red,
+                activeIcon: Icon(Icons.movie),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.find_in_page_outlined),
+                activeIcon: Icon(Icons.find_in_page),
+                label: 'buscar',
+              )
+            ],
+            selectedFontSize: 16.0,
+            currentIndex: _selectedTabIndex,
+            selectedItemColor: Colors.yellow[700],
+            elevation: 1.0,
+            iconSize: 30.0,
+            unselectedItemColor: Colors.grey[400],
+            onTap: _onBottomMenuTapped,
+            // showSelectedLabels: false,
+            showUnselectedLabels: false,
+          ),
+        ),
+      ),
     );
   }
 
