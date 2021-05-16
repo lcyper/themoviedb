f1f37a1d99740bc19db6c27240400839fa8c69bb
lcyper <lcyper@gmail.com>
2021-05-16 16:16:54 -0300

    archivos obsoletos

--- lib/helpers/shared_preferences.dart
+++
@@ -1,9 +0,0 @@
- import 'package:shared_preferences/shared_preferences.dart';
-
-SharedPreferences _preferences;
-  Future<SharedPreferences> get preferences async {
-    if (_preferences == null) {
-      _preferences = await SharedPreferences.getInstance();
-    }
-    return _preferences;
-  }
