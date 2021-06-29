ea790bb024f56920f45e10b79c6643948886ed55
lcyper <lcyper@gmail.com>
2021-06-29 00:42:53 -0300

    refactoring

--- lib/screens/widgets/createListView.dart
+++ lib/screens/widgets/createListView.dart
@@ -16,37 +16,39 @@ Widget createListView(List<Movies> movies, {String type}) {
     itemBuilder: (context, index) {
       if (type == "favorite") {
         //es caso de que sea en la pagina de favoritos
-        return Dismissible(
-          key: Key(index.toString()),
-          child: CreateCard(movie: movies[index]),
-          background: Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.only(right: 15.0, top: 20.0),
-            child: Icon(
-              Icons.delete,
-              color: Colors.red,
-              size: 30.0,
-            ),
-          ),
-          direction: DismissDirection.endToStart,
-          onDismissed: (direction) {
-            if (direction == DismissDirection.endToStart) {
-              Provider.of<CacheApp>(context, listen: false).toggleFavorite =
-                  movies[index];
-              // Movies().toggleFavorite(id: movies[index].id);
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content:
-                      Text(movies[index].title + ' eliminada de Favoritos'),
-                  duration: Duration(seconds: 2),
-                ),
-              );
-              return movies.removeAt(index);
-            }
-          },
-        );
+        return _dismissible(index, movies, context);
       }
       return CreateCard(movie: movies[index]);
     },
   );
 }
+
+Dismissible _dismissible(int index, List<Movies> movies, BuildContext context) => Dismissible(
+        key: Key(index.toString()),
+        child: CreateCard(movie: movies[index]),
+        background: Container(
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 15.0, top: 20.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.red,
+            size: 30.0,
+          ),
+        ),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) {
+          if (direction == DismissDirection.endToStart) {
+            Provider.of<CacheApp>(context, listen: false).toggleFavorite =
+                movies[index];
+            // Movies().toggleFavorite(id: movies[index].id);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content:
+                    Text(movies[index].title + ' eliminada de Favoritos'),
+                duration: Duration(seconds: 2),
+              ),
+            );
+            return movies.removeAt(index);
+          }
+        },
+      );
